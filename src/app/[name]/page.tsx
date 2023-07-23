6aaeaa421a172894db39d21687e09d23b35d249a
Rehan Ali Wakkas <63908192+rehanaliwaqas@users.noreply.github.com>
2023-07-23 18:48:56 +0300

    updated

---
+++ src/app/[name]/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(props:any){
+  console.log(props)
+  //let params:{name:""}
+  return(
+    <main className ="flex justify-center items-center flex-col p-2">
+      <h1 className="text-[36px] mb-6">Dynamic Page</h1>
+    </main>
+  )
+}
