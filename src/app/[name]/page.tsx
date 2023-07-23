544769733aa5a93f7f6bf8a47586e01abe633790
Rehan Ali Wakkas <63908192+rehanaliwaqas@users.noreply.github.com>
2023-07-23 18:55:16 +0300

    Update page.tsx

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -2,8 +2,10 @@ export default function Home(props:any){
   console.log(props)
   //let params:{name:""}
   return(
+  
     <main className ="flex justify-center items-center flex-col p-2">
       <h1 className="text-[36px] mb-6">Dynamic Page</h1>
+      <h1>AlRyhans Systems</h1>
     </main>
   )
 }
